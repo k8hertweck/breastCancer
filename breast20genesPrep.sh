d173a45499fa783a7b5fc2aa2e33f9565e20d94f
k8hertweck <k8hertweck@gmail.com>
2015-10-28 16:04:07 -0500

    changing directory structure

--- breast20genesPrep.sh
+++ breast20genesPrep.sh
@@ -2,16 +2,19 @@
 
 ## summarization of 20 gene breast cancer data
 
+WORK=~/Copy/cancerGenomics/20genesBreastCancer/
 FILTERED=*filtered*.tsv
 
-cd ~/Copy/cancerGenomics/20genesBreastCancer/paired_comparison_10262015/filtered
+cd $WORK
+mkdir processing
+cd paired_comparison_10262015/filtered
 
 # make list of genes
 echo -n genesAll.txt
-for x in */$FILTERED; do tail +2 $x | cut -f 8 | sort | uniq >> genesAll.txt; done
-sort genesAll.txt | uniq > geneNames.txt
+for x in */$FILTERED
+	do 
+		tail +2 $x | cut -f 8 | sort | uniq >> genesAll.txt
+done
+
+sort genesAll.txt | uniq > $WORK/processing/geneNames.txt
 rm genesAll.txt
-
-#replace LOC101928103 with BARD1 in all files 
-
-
