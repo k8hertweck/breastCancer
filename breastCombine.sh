65ee7fdb8371844f8a80dd8f882bdbedb754a506
k8hertweck <k8hertweck@gmail.com>
2016-02-25 16:53:52 -0600

    fixing header echo

---
+++ breastCombine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## combining breast cancer data from multiple patients
+
+SCRIPT=`pwd`
+WORK=~/Dropbox/cancerGenomics/breast
+
+cd $WORK 
+mkdir combined
+
+# combine unique SNPs from different individuals
+cd $WORK/processing/processed
+echo -n > temp
+for x in *.tsv
+	do
+		tail +2 $x | cut -f 1-6,8 | sort | uniq >> temp
+done
+
+echo -e 'chromosome\tstart\tend\tref\talt\tdbsnp\tGene' > ../combined/combVariants.lst
+sort temp >> ../combined/combVariants.lst
+rm temp
+
+# count number of individuals with each variant
+uniq -c ../combined/combVariants.lst > ../combined/countVariants.lst
+
+# obtain list of unique variants
+uniq ../combined/combVariants.lst > ../combined/uniqVariants.lst
