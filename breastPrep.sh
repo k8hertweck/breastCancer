84e816b2accc4c3281a68f344ff446ba78689047
k8hertweck <k8hertweck@gmail.com>
2016-10-09 13:49:03 -0500

    correcting directories

--- breastPrep.sh
+++ breastPrep.sh
@@ -3,11 +3,10 @@
 ## preparation of target gene breast cancer data
 
 SCRIPT=`pwd`
-WORK=~/Copy/cancerGenomics/breast
+WORK=~/Dropbox/cancerGenomics/breast
 
 cd $WORK
 mkdir processing
-rm processing*
 
 # create header
 head -1 paired_comparison_10262015/filtered/BC01/BC01_OtA9915_vs_OtA9931_text_report_filtered_10262015.tsv > processing/header.tsv
@@ -22,10 +21,11 @@ for x in `cat $SCRIPT/sampleNames.lst`
 			do
 				grep $'\t'$gene$'\t' *text_report_filtered_10262015.tsv >> $WORK/processing/$x.tsv
 		done
-		cd $WORK/paired_comparison_10262015/processed
+		cd $WORK/paired_comparison_10262015/filtered
 done
 
 # tidy up 
+cd $WORK/processing
 rm header.tsv
-mkdir filtered
-mv *.tsv filtered
+mkdir processed
+mv *.tsv processed
